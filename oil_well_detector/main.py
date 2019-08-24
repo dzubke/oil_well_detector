b745f4bfe6472c66e0436e39e875c4db689a4337
Dustin Zubke <dzubke@outlook.com>
2019-08-24 16:54:30 -0700

    testing my commits are recorded

--- oil_well_detector/main.py
+++ oil_well_detector/main.py
@@ -89,3 +89,5 @@ if __name__ == '__main__':
 
     # as I somewhat expected, the nn is always predicting a zero value because there are so few positive 
     # values. I need to think about how to proceed....
+
+    # adding some values to test git
